05ece3211ec1ebc612151c305d81e48d7ee8c2c2
danielgonzalez22 <kavellid63@gmail.com>
2022-09-01 03:57:23 -0300

    comentarios quitados, codigo 100% en inglés

--- config/database.js
+++ config/database.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose')
-  mongoose.connect(               //este metodo connect devuelve una promesa//
-      //primer parametro: link de conexion//
-      //segundo parametro: objecto con dos propiedades en true//
-  process.env.MONGO_URI,         //primer parametro, definido en .env//
-  {                              //segundo parametro, objeto con estas 2 propiedades en true://
-      useUnifiedTopology: true,  //para controlar mongodb con mongoose
-      useNewUrlParser: true      //para utilizar el analizador de errores de mongoose en lugar del de mongo
+  mongoose.connect(               
+  process.env.MONGO_URI,         
+  {                              
+      useUnifiedTopology: true,  
+      useNewUrlParser: true      
   }
 )
-   .then(()=>console.log('connected to database successfully')) //aviso de exito en la conexion//
-   .catch(error=>console.log(error)) //ya que devuelve una promesa, se debe controlar algun error//
+   .then(()=>console.log('connected to database successfully')) 
+   .catch(error=>console.log(error)) 
