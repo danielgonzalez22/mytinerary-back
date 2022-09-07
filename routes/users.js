edfe742aaaa5b809ac5286f0e5c6f1e378992753
Mauro Schork <mauroschork107@hotmail.com>
2022-09-07 16:43:12 -0300

    se agregan rutas de comments, itinerary y users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { getUser, createUser } = require('../controllers/UserController' )
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+
+router.get('/:id', getUser );
+router.post('/', createUser);
 
 module.exports = router;
