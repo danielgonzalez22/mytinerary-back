055fae476bafa72f1801915e884d9fadcd772fb8
David Chaban <davidchaban01@gmail.com>
2022-09-08 16:03:48 -0300

    Se agrega Mocka, Chai y test de ciudades y itineraries

--- test/citiesTest.js
+++ test/citiesTest.js
@@ -1,65 +1,47 @@
-const request = require('supertest')
-const app = require('../app')
-const { deleteOne } = require('../models/City')
+const request = require("supertest");
+const app = require("../app");
+const { assert } = require("chai");
+let cityId = ""
+describe("POST /cities", function () {
+  it("Must respond with to 201", function (done) {
+    request(app)
+      .post("/cities")
+      .send({
+        "city": "Dubai",
+        "country": "United Arab Emirates",
+        "photo": "http://localhost:400/image",
+        "population": 3310000,
+        "foundation": 1883,
+      })
+      
+      .then((response) => {
+        cityId= response.body.response
+        assert.isString(response.body.response)
+       done()
+      })
+      
+  });
+});
+describe("DELETE /cities", function () {
+  it("Must respond with 200 status code", function (done) {
+    request(app)
+      .delete(`/cities/${cityId}`)
+      .expect(200)
+      .end(function (err, res) {
+        if (err) return done(err);
+        return done();
+      });
+  });
+});
 
-
-
-describe('POST /events', function () {
-    it('Must respond with 201 status code', function (done) {
-        request(app)
-            .post('/cities')
-            .send({
-                city: "Rome",
-                country: "Italy",
-                photo: "http://localhost:400/image",
-                population: 2500,
-                foundation: 1810-05-01,
-                description: "muchtext"
-            })
-        
-            .expect(201, done )// exitoso
-          
-    })
-
-    it('Must respond with 400 status code', function (done) {
-        request (app)//
-            .post('/cities')
-            .send({})
-            .expect(400)
-            .end(function(err, res ) {
-                if (err) return done(err);
-                return done();
-
-            })
-            .expect(400, done )
-    })
-
-    describe('GET /cities', function () {
-
-
-    })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-})
-
-
-
-
-
-
-
-
+describe("GET /cities", function () {
+  it("Must respond with 200 status code", function (done) {
+    request(app)
+      .get("/cities")
+      .expect(200)
+      .end(function (err, res) {
+        if (err) return done(err);
+        return done();
+      });
+  });
+});
