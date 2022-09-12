14309f38b328cd498dfad534bffc3b7b5f3272cd
danielgonzalez22 <kavellid63@gmail.com>
2022-09-11 23:43:40 -0300

    ruta de controlador de comments corregida

--- routes/comments.js
+++ routes/comments.js
@@ -1,9 +1,10 @@
+const { Router } = require('express');
 var express = require('express');
 var router = express.Router();
-const {create, read } = require('../controllers/CommentController.js')
+const { create, getComments } = require('../controllers/CommentController.js')
 
-router.post('/',create);
-
-router.get('/:id', read);
+router.post('/', create);
+router.get('/', getComments)
+// router.get('/:id', read);
 
 module.exports = router;
